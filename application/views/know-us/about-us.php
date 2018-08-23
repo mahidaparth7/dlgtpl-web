25a0db6fd7dd7b5b155ddb3966c147a648afb170
DESKTOP-EB27I2G\Admin <mahidaparth7@gmail.com>
2018-08-23 23:10:39 +0530

    changes

--- application/views/know-us/about-us.php
+++ application/views/know-us/about-us.php
@@ -89,7 +89,7 @@
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-md-4 ">
+				<div class="col-md-3 ">
 				</div>
 				<div class="col-md-6 ">
 					<img class="img-rounded img-responsive img-raised" alt="Raised Image" src="<?php echo base_url();?>assets/img/dl-house/day-scene.jpg">
